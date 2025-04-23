e3365cb7566c8eebcaedab4bdacefcb87bdec7da
Yasitha Herath <35001977+amodhyh@users.noreply.github.com>
2025-04-23 11:39:50 +0530

    Update firebaseConfig.js

--- tars/config/firebaseConfig.js
+++ tars/config/firebaseConfig.js
@@ -6,13 +6,13 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 // Your web app's Firebase configurat
 const firebaseConfig = {
-  apiKey: "AIzaSyCYNClhZgsqVfwuQMamAfDXpH3x0ulQlqs",
-  authDomain: "tars-90cbb.firebaseapp.com",
-  databaseURL: "https://tars-90cbb-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "tars-90cbb",
-  storageBucket: "tars-90cbb.firebasestorage.app",
-  messagingSenderId: "165564318019",
-  appId: "1:165564318019:web:ed40891e87ee8822672ff6"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
